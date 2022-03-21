cb0faa13cf6827ea6bf8c68ef14f4b49cfaf8173
Daniel <daniel.panero@gmail.com>
2022-03-21 16:35:49 +0100

    added txt tests

---
+++ tests/run-txt.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+folder="tests/test_txt/"
+files=(
+    "error_defensor_not_within_home.txt"
+    "error_homes_overlap.txt"
+    "t01.txt"
+    "t02.txt"
+    "t03.txt"
+    "t04.txt"
+    "t05.txt"
+    "t06.txt"
+    "t07.txt"
+    "t08.txt"
+    "t09.txt"
+    "t10.txt"
+    "t11.txt"
+    "t12.txt"
+    "t13.txt"
+    "t14.txt"
+)
+expected=(
+    "defensor with coordinates 101 117 is not fully within its home: 0"
+    "home 0 overlaps with home 1"
+    "collector with coordinates 14 18 overlaps with another exclusive entity at least on 15 17"
+    "collector with coordinates 18 14 overlaps with another exclusive entity at least on 17 15"
+    "defensor with coordinates 101 117 is not fully within its home: 0"
+    "defensor with coordinates 118 102 is not fully within its home: 0"
+    "defensor with coordinates 118 117 is not fully within its home: 0"
+    "defensor with coordinates 101 102 is not fully within its home: 0"
+    "defensor with coordinates 106 106 overlaps with another exclusive entity at least on 105 105"
+    "food with coordinates 78 78 overlaps with another exclusive entity"
+    "generator with coordinates 3 3 overlaps with another exclusive entity at least on 4 5"
+    "predator with coordinates 22 24 overlaps with another exclusive entity"
+    "generator with coordinates 11 5 is not fully within its home: 1"
+    "home 0 overlaps with home 1"
+    "coordinate 10000000 does not belong to [ 0, 127 ]"
+    "combined coordinate 117 and square side 12 do not belong to [ 0, 127 ]"
+)
+
+total=0
+failed=0
+
+echo Compiling projects files
+cd ../
+make
+
+for i in "${!files[@]}"; do
+    echo -e "\e[1;37m""$i": "${files[$i]}""$(tput sgr0)"
+
+    output=$(./project "${folder}""${files[$i]}")
+    total=$((total + 1))
+
+  if ! [[ "${output}" == *"${expected[$i]}"* ]]; then
+        echo "./project ${folder}${files[$i]}"
+
+        echo -e "\e[1;37m""Expected":
+        echo -e "\e[1;31m""${expected[$i]}""\e[0;m"
+        echo -e "\e[1;37m""Received":
+        echo -e "\e[1;31m""${output}""\e[0;m"
+        echo
+        echo
+
+        failed=$((failed + 1))
+  fi
+
+done
+
+echo 
+if (( $(echo "${failed} > 0" | bc -l) )); then
+    echo -e "\e[1;31m""Failed: " "${failed} / ${total}"
+else
+    echo -e "\e[1;32mFinished:" "${total} / ${total}" 
+fi
